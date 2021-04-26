647cdfbd341958fad915d1beee19d5c655cb92fb
felipemoslavacz <felipe.limah@gmail.com>
2021-04-26 12:30:51 -0300

    chore: remove useless constructor

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -2,12 +2,7 @@ import React, {Component} from "react";
 import "./index.css";
 
 export default class ProductList extends Component {
-    constructor() {
-        super();
-    }
-
     render() {
-
         return (
             <div className="layout-row wrap justify-content-center flex-70 app-product-list">
                 {this.props.products.map((product, i) => {
